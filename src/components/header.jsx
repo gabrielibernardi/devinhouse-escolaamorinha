6ace2c80290463254688628f39e3688079297133
Gabrieli Bernardi <gabrieli.bernardi@gmail.com>
2021-01-05 13:43:51 -0300

    header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,8 @@ import "../components/header.css";
 function Header() {
     return (
         <div className="header">
-            <p>Escola Amorinha</p>
+            <p>Escola Amorinha |</p>
+            <span>Controle de aluno(a)s</span>
         </div>
     )
 
